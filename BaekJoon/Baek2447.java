1b12630ae45fbe935dfa83af0bb22d177ed70ee6
김윤정 <cairofaith@gmail.com>
2020-08-21 23:49:40 +0900

    upload

---
+++ BaekJoon/Baek2447.java
@@ -0,0 +1,44 @@
+//Recursion
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Baek2447{
+
+	public static char fig[][];
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		fig = new char[n][n];
+		
+		for(char[] row: fig){
+			Arrays.fill(row, ' ');
+		}
+
+		drawFig(0, 0, n);
+
+		for(char[] row: fig){
+			System.out.println(row);
+		}
+
+		return;
+	}
+
+	public static void drawFig(int a, int b, int num){
+		if(num == 1){
+			fig[a][b] = '*';
+			return;
+		}
+		int n = num/3;
+
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				if(i==1 && j==1)
+					continue;
+				drawFig((a*3)+i,(b*3)+j,n);
+			}
+		}
+		return;
+	}
+}
---
+++ BaekJoon/Baek2565.java
@@ -0,0 +1,41 @@
+//Dynamic Programming
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[][] input = new int[n][2];
+    int[] inc = new int[n];
+
+    for(int i=0;i<n;i++){
+      input[i][0] = sc.nextInt();
+      input[i][1] = sc.nextInt();
+    }
+    sc.close();
+
+    Arrays.sort(input, new Comparator<int[]>() {
+      public int compare(int[] t1, int[] t2){
+        if(t1[0]==t2[0])
+          return t1[1]-t2[1];
+        else
+          return t1[0]-t2[0];
+      }
+    });
+
+    inc[0] = 1;
+    for(int i=1;i<n;i++){
+      inc[i] = 1;
+      for(int j=0;j<i;j++){
+        if(input[i][0]>input[j][0] && input[i][1]>input[j][1] && inc[i]<inc[j]+1){
+          inc[i] = inc[j]+1;
+        }
+      }
+    }
+
+    Arrays.sort(inc);
+    System.out.println(n-inc[n-1]);
+  }
+}
