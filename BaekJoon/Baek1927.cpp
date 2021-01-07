4ab36b702cd2b6f7c10f181aef3724140534b57e
yunjung <cairofaith@gmail.com>
2021-01-07 23:28:04 +0900

    upload

---
+++ BaekJoon/Baek1927.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	int n = 0;
+	scanf("%d", &n);
+
+	priority_queue<int, vector<int>, greater<int> > pq; 
+	int num;
+
+	for (int i = 0 ; i < n; i++) {
+		scanf("%d", &num);
+		if (num == 0) {
+			if(pq.empty()) {
+				printf("0\n");
+			} else {
+				printf("%d\n", pq.top());
+				pq.pop();
+			}
+		} else { 
+			pq.push(num);
+		}
+	}
+
+	return 0;
+}
