937428d6830977a493744bd455e164cae00a2dd4
yunjung <cairofaith@gmail.com>
2021-01-09 22:05:13 +0900

    upload

---
+++ BaekJoon/Baek1520.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+void dfs() {
+    
+}
+
+int main() {
+    int m = 0;
+    int n = 0;
+
+    scanf("%d %d", &m, &n);
+
+    vector<vector<int> > route (m, vector<int>(n));
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &route[i][j]);
+        }
+    }
+
+    return 0;
+}
